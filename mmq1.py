c56ddbe2210b306aec2a1035d76e0d3ab155e0a9
AziD Shroud <adamdespoina2004@gmail.com>
2025-08-19 17:44:11 +0300

    made a video based on the difference frames

--- mmq1.py
+++ mmq1.py
@@ -1,6 +1,13 @@
 import cv2
 import os
+
 folder= "./Frame_Folder"
+width= 1920
+height= 1080
+
+def get_frame_number(filename):
+    return int(filename.replace('frame', '').replace('.jpg', ''))
+
 cap = cv2.VideoCapture('./Social_Network.avi')
 count = 0
 while cap.isOpened():
@@ -8,21 +15,19 @@ while cap.isOpened():
     if not ret:
         print("End of video or failed to read the frame.")
         break
-    #  cv2.imshow('window-name', frame)
+   
     cv2.imwrite("./Frame_Folder/frame%d.jpg" % count, frame)
     count = count + 1
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+    
 
-cap.release()
-cv2.destroyAllWindows() # destroy all opened windows    
+cap.release() 
+
+# Read Files
 files = [f for f in os.listdir(folder)]
-
-def get_frame_number(filename):
-    return int(filename.replace('frame', '').replace('.jpg', ''))
-
-# Sort files
 files.sort(key=get_frame_number)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+video = cv2.VideoWriter('Social_Net_diff.avi', fourcc, 24, (width, height),False)
 
 for i in range(len(files) - 1):
     img1 = cv2.imread(os.path.join(folder, files[i]), cv2.IMREAD_GRAYSCALE)
@@ -30,4 +35,4 @@ for i in range(len(files) - 1):
 
     diff = cv2.absdiff(img2, img1)
     cv2.imwrite("./Diff_Folder/Diff%d.jpg" % i, diff)
-    
+    video.write(diff)
